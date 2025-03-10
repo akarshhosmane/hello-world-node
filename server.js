5684b096b1b5b22e20e84a7f409bc9bb69bc1552
akarshhosmane <43984210+akarshhosmane@users.noreply.github.com>
2025-03-10 12:41:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const app = express()
 const port = 5000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!-7')
+  res.send('Hello World!-8')
 })
 
 app.listen(port, () => {
-  console.log(`Example app7 listening at http://localhost:${port}`)
+  console.log(`Example app8 listening at http://localhost:${port}`)
 })
